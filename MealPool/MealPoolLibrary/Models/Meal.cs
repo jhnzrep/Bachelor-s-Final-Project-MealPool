4fb98e14de1497d11240fb4a75b57bbe4581c41f
jhnzrep <jpc@stromlin.com>
2022-04-08 19:40:55 +0200

    Added Models to class library

---
+++ MealPool/MealPoolLibrary/Models/Meal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPoolLibrary.Models
+{
+    class Meal
+    {
+        public int Id { get; set; }
+        public int CookId { get; set; }
+        public string Name { get; set; }
+        DateTime Created { get; set; }
+        DateTime DateTime { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+
+    }
+}
---
+++ MealPool/MealPoolLibrary/Models/Rating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPoolLibrary.Models
+{
+    class Rating
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public int RatedId { get; set; }
+        public int Stars { get; set; }
+        public string Comment { get; set; } 
+    }
+}
