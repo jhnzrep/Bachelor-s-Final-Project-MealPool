2b570d06cb8f131f5a07c1d879aa6cee6729e86d
Matúš Kalanin <mato.kalanin@gmail.com>
2022-04-14 20:48:58 +0200

    Login page - mobile design

--- MealPool/mealpool_frontend/src/pages/login/loginPage.tsx
+++ MealPool/mealpool_frontend/src/pages/login/loginPage.tsx
@@ -10,6 +10,38 @@ class LoginPage extends Component {
                     <img className={styles.main_logo} src={Logo} alt='' />
                     <h1>MEALPOOL</h1>
                 </div>
+                
+                <form action="">
+                    <div className={styles.form_wrapper}>
+                        <h4>Sign In</h4>
+                        <div className={styles.Row}>
+                            <input type="email" id="email" name="email" placeholder="Email account"/>
+                        </div>
+                        <div className={styles.Row}>
+                            <input type="password" id="password" name="password" placeholder="Password"/>
+                        </div>
+                        <div className={styles.rememberMeInput}>
+                            <input type="checkbox" id="rememberMeInput" name="rememberMeInput" value="Bike"/>
+                            <label htmlFor="rememberMeInput"> Remember me? </label>
+                        </div>
+                        <div className={styles.Row}>
+                            <input className={styles.submit_input} type="submit" value="Sign In"/>
+                        </div>
+                    </div>
+                </form>
+
+                    <div className={styles.alternative_login_section}>
+                        <div className={styles.line_decorated_text}>
+                            <hr /> 
+                            <span>
+                                OR
+                            </span>
+                            <hr />
+                        </div>
+                        <div className={styles.new_account_link}>
+                            <a href='#'>Create a new account</a>
+                        </div>
+                    </div>
             </div>
         )
     }
