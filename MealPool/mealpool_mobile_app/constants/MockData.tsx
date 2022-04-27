1bdefa4e4b98ed55c9afdcefd5b2d096e6420803
Matúš Kalanin <mato.kalanin@gmail.com>
2022-04-27 20:10:54 +0200

    Mock data file

---
+++ MealPool/mealpool_mobile_app/constants/MockData.tsx
@@ -0,0 +1,41 @@
+ const DATA = [
+    {
+        title: "Tasty breakfast with avocado",
+    },
+    {
+        title: "Tasty breakfast with avocado2",
+    },
+    {
+        title: "Tasty breakfast with avocado3",
+    }
+];
+
+ const food_category_mock_data = [
+    {
+        title: "Indian",
+        image: "../assets/images/meal_category_image.svg"
+    },
+    {
+        title: "Vegetarian",
+        image: "../assets/images/meal_category_image.svg"
+    },
+    {
+        title: "Fast food",
+        image: "../assets/images/meal_category_image.svg"
+    },
+    {
+        title: "Italian",
+        image: "../assets/images/meal_category_image.svg"
+    },
+    {
+        title: "Sushi",
+        image: "../assets/images/meal_category_image.svg"
+    },
+    {
+        title: "Sushi",
+        image: "../assets/images/meal_category_image.svg"
+    },
+    
+];
+
+export  {food_category_mock_data, DATA }
